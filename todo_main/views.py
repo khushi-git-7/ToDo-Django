30b86a0039418bba95ff1300c890bcfa114afc63
Khushi <khushi@gmail.com>
2026-04-29 21:55:27 +0530

    changes in the ui

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,6 +3,7 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    return render(request, 'home.html', {'tasks': tasks})
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    return render(request, 'home.html', {'tasks': tasks, 'completed_tasks': completed_tasks})
 
 
